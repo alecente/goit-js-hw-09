a4231ab3faa743e1f01387d4068b7667e8bd95f5
GG_Ray <110432497+alecente@users.noreply.github.com>
2023-05-06 21:59:21 -0500

    timer 07

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,14 +3,13 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
-const refs = {
-startBtn: document.querySelector('[data-start]'),
-daysRef: document.querySelector('[data-days]'),
-hoursRef: document.querySelector('[data-hours]'),
-minutesRef: document.querySelector('[data-minutes]'),
-secondsRef: document.querySelector('[data-seconds]'),
-};
+const startBtn = document.querySelector('[data-start]');
+const daysRef = document.querySelector('[data-days]');
+const hoursRef = document.querySelector('[data-hours]');
+const minutesRef = document.querySelector('[data-minutes]');
+const secondsRef = document.querySelector('[data-seconds]');
 let timerId = null;
+
 
 startBtn.setAttribute('disabled', true);
 
